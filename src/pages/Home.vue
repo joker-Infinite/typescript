4cba31f0c8e5a14808a43b81c078a37739d9feab
king <2075259561@qq.com>
2021-01-05 09:27:20 +0800

    up

---
+++ src/pages/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div style="width: 100%;height: 100%">111</div>
+</template>
+
+<script lang="ts">
+    import {Vue, Component} from 'vue-property-decorator'
+
+    @Component({
+        components: {}
+    })
+    export default class Home extends Vue {
+    }
+</script>
+
+<style scoped>
+
+</style>
